9eb662f89a86b72f7818c43eeb406b3841637628
solmar <solmar@dexeus.com>
2021-05-25 00:47:08 +0200

    generate outputs

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -9,14 +9,25 @@ class GameTest extends TestCase
     protected function setUp()
     {
         $this->createGoldenMaster();
+        $this->generateOutputs(5);
     }
 
-    public function testShouldCreateGoldenMaster() {
-        $this->assertTrue(file_exists (self::GOLDEN_MASTER));
+    public function testShouldCreateGoldenMaster()
+    {
+        $this->assertTrue(file_exists(self::GOLDEN_MASTER));
     }
 
     private function createGoldenMaster()
     {
         file_put_contents(self::GOLDEN_MASTER, "");
+    }
+
+    private function generateOutputs(int $seed)
+    {
+        do {
+            exec("php ../src/GameRunner.php", $output);
+            file_put_contents(self::GOLDEN_MASTER, join($output, PHP_EOL), FILE_APPEND);
+            --$seed;
+        } while ($seed > 0);
     }
 }
